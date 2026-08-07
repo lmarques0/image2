e85a47caf5575040cde03eaf9f9f6b52d85890a7
lucass <l.marques@estudante.ifmt.edu.br>
2026-08-07 12:02:14 -0400

    Paginas Typescript

---
+++ imagelite/app/page.tsx
@@ -0,0 +1,12 @@
+import { PrimeiroComponente } from "./components/PrimeiroComponente";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <PrimeiroComponente/>
+      </main>
+    </div>
+
+  )
+}
